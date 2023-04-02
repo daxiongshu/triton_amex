96e6a4c74ed3800492eae36db753c308dc10c7dd
jiweil <jiweil@nvidia.com>
2023-04-02 08:15:31 -0700

    launch triton in notebook

--- docker.sh
+++
@@ -1,4 +0,0 @@
-docker run -p 8000:8000 -p 8001:8001 --gpus device=0 \
-  -v ${PWD}:/models \
-  nvcr.io/nvidia/tritonserver:23.03-py3 \
-  tritonserver --model-repository=/models --exit-on-error=false
